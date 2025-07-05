e6f160617e325fba46b7c62ff462e1e2e303746c
Avijit Karmaker <ajkacca@gmail.com>
2025-07-06 00:52:33 +0300

    add browser router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,33 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react'
 import './App.css'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+import Home from './pages/Home.jsx'
+import Shows from './pages/Shows.jsx'
+import ShowDetails from './pages/ShowDetails.jsx'
+import SeatLayout from './components/SeatLayout.jsx'
+import Bookings from './pages/Booking.jsx'
+import Favorite from './pages/Favorite.jsx'
+import Navbar from './components/Navbar.jsx'
+import Footer from './components/Footer.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/shows",
+    element: <Shows />,
+  },
+  {
+    path: "/shows/:id",
+    element: <ShowDetails />,
+  },
+  {
+    path: "/shows/:id/:date",
+    element: <SeatLayout />,
+  },
+  {
+    path: "/my-bookings",
+    element: <Bookings />,
+  },
+
+  { 
+    path: "/my-favorite",
+    element: <Favorite />,
+  },
+])
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Navbar />
+      <RouterProvider router={router} />
+      <Footer />  
     </>
   )
 }
