154d9096da0eb00cd68f2395193d1c534293981b
Avijit Karmaker <ajkacca@gmail.com>
2025-07-07 11:03:05 +0300

    add tailwind, daisy ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,3 @@
-import React from 'react'
-import './App.css'
 import {createBrowserRouter, RouterProvider} from 'react-router-dom'
 import Home from './pages/Home.jsx'
 import Shows from './pages/Shows.jsx'
