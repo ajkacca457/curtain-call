c29f6a3be05bd7598315308aba2a50dec5924a79
Avijit Karmaker <ajkacca@gmail.com>
2025-07-07 11:17:31 +0300

    update mobile navigation

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -28,21 +28,16 @@ const Navbar = () => {
               className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
             >
               <li>
-                <a>Item 1</a>
+                <Link to="/">Home</Link>
               </li>
               <li>
-                <a>Parent</a>
-                <ul className="p-2">
-                  <li>
-                    <a>Submenu 1</a>
-                  </li>
-                  <li>
-                    <a>Submenu 2</a>
-                  </li>
-                </ul>
+                <Link to="/shows">Shows</Link>
               </li>
               <li>
-                <a>Item 3</a>
+                <Link to="/my-bookings">My Bookings</Link>
+              </li>
+              <li>
+                <Link to="/my-favorite">My Favorite</Link>
               </li>
             </ul>
           </div>
