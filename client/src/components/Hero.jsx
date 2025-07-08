91970248de3637793a1572c43df58eca5e6b3aba
Avijit Karmaker <ajkacca@gmail.com>
2025-07-08 11:42:06 +0300

    update hero colors

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -8,14 +8,22 @@ const Hero = () => {
       style={{ backgroundImage: `url(${HeroImg})` }}
     >
       {/* Overlay */}
-      <div className='absolute inset-0 bg-black opacity-60 rounded-2xl'></div>
+      <div className='absolute inset-0 bg-black opacity-70 rounded-2xl'></div>
 
       {/* Content */}
       <div className='relative z-10 flex flex-col items-end justify-end h-full p-8'>
-        <h4 className='text-white font-bold text-lg tracking-wider drop-shadow-md'>
-          NOW SHOWING:
+        <h4 className='text-purple-300 font-bold text-lg tracking-wider drop-shadow-md mb-4'>
+          NOW SHOWING
         </h4>
-        <h1 className='text-white text-4xl font-semibold tracking-wide drop-shadow-md'>
+               {/* Small details */}
+        <div className='text-gray-300 text-sm tracking-wide space-x-4 mb-2 drop-shadow-sm text-right'>
+          <span className='text-purple-300'>Genre: Musical Drama</span>
+          <span className='text-purple-300'>•</span>
+          <span className='text-purple-300'>Runtime: 2h 30m</span>
+          <span className='text-purple-300'>•</span>
+          <span className='text-purple-300'>Writer: Andrew Lloyd Webber</span>
+        </div>
+        <h1 className='text-purple-400 text-4xl font-semibold tracking-wide drop-shadow-md'>
           Phantom of the Opera
         </h1>
         <p className='mt-4 text-md text-gray-200 max-w-xl drop-shadow-sm text-right'>
@@ -24,10 +32,10 @@ const Hero = () => {
         </p>
 
         <div className='mt-6 flex gap-4'>
-          <button className='bg-transparent border border-white text-white px-6 py-2 rounded-full text-sm hover:bg-white hover:text-black transition cursor-pointer'>
+          <button className='bg-transparent border border-purple-400 text-purple-400 px-6 py-2 rounded-full text-sm hover:bg-purple-400 hover:text-black transition cursor-pointer'>
             Book Tickets
           </button>
-          <button className='bg-transparent border border-white text-white px-6 py-2 rounded-full text-sm hover:bg-white hover:text-black transition cursor-pointer'>
+          <button className='bg-transparent border border-purple-400 text-purple-400 px-6 py-2 rounded-full text-sm hover:bg-purple-400 hover:text-black transition cursor-pointer'>
             Watch Trailer
           </button>
         </div>
