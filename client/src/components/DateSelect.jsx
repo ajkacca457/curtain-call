a76714e7e3de04a74135fee408316354cc01406d
Avijit Karmaker <ajkacca@gmail.com>
2025-07-11 13:39:05 +0300

    add dateselect

--- client/src/components/DateSelect.jsx
+++ client/src/components/DateSelect.jsx
@@ -1,23 +1,36 @@
 import { FaArrowCircleLeft, FaArrowCircleRight } from "react-icons/fa";
 
-const DateSelect = ({dateTime,id}) => {
+const DateSelect = ({ dateTime, id }) => {
   return (
     <div className="py-8">
-        <h2 className="text-xl font-semibold mb-4">Select show date: </h2>
+      <h2 className="text-xl font-semibold mb-4">Select show date: </h2>
+      <div className="flex justify-between items-center">
         <div className="flex mx-auto justify-between items-center">
-           <FaArrowCircleLeft size={25}/>
-            <div>
-                {Object.keys(dateTime).map((item)=> {
-                    return  <button key={item} className="bg-green-200 px-4 py-6 m-2 border-2 border-amber-200">
-                        <span className="text-xl">{new Date(item).getDate()}</span>
-                        <span className="ml-2 text-xl">{new Date(item).toLocaleDateString("en-US", {month:"short"})}</span>
-                    </button>
-                })}
-            </div>
-           <FaArrowCircleRight size={25}/>     
+          <FaArrowCircleLeft size={25} />
+          <div>
+            {Object.keys(dateTime).map((item) => {
+              return (
+                <button
+                  key={item}
+                  className="bg-green-200 px-4 py-6 m-2 border-2 border-amber-200"
+                >
+                  <span className="text-xl">{new Date(item).getDate()}</span>
+                  <span className="ml-2 text-xl">
+                    {new Date(item).toLocaleDateString("en-US", {
+                      month: "short",
+                    })}
+                  </span>
+                </button>
+              );
+            })}
+          </div>
+          <FaArrowCircleRight size={25} />
         </div>
-    </div>
-  )
-}
 
-export default DateSelect
+        <button>Book Show</button>
+      </div>
+    </div>
+  );
+};
+
+export default DateSelect;
