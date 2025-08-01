7da9486023c3f22170f6018a7a89ad4219661094
Avijit Karmaker <ajkacca@gmail.com>
2025-08-01 10:08:24 +0300

    update inngest functions

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -10,10 +10,10 @@ const syncUserCreation= inngest.createFunction(
     {id:"sync-user-from-clerk"},
     {event:"clerk/user.created"},
     async ({event})=> {
-        const {id, first_name, last_name,email_address,image_url}=event.data;
+        const {id, first_name, last_name,email_addresses,image_url}=event.data;
         const userData= {
             _id:id,
-            email: email_address[0].email_address,
+            email: email_addresses[0].email_address,
             name : `${first_name} ${last_name}`,
             image: image_url
         }
@@ -40,10 +40,10 @@ const syncUserUpdate=inngest.createFunction(
     {id:"update-user-with-clerk"},
     {event:"clerk/user.updated"},
     async({event})=> {  
-        const {id, first_name, last_name,email_address,image_url}=event.data;
+        const {id, first_name, last_name,email_addresses,image_url}=event.data;
         const userData= {
             _id:id,
-            email: email_address[0].email_address,
+            email: email_addresses[0].email_address,
             name : `${first_name} ${last_name}`,
             image: image_url
         }
