988b7482582a0915ccac0880c84b2893cdf4e10a
Avijit Karmaker <ajkacca@gmail.com>
2025-08-01 06:13:43 +0300

    update inngest file

--- server/index.js
+++ server/index.js
@@ -2,15 +2,21 @@ import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
 import ConnectDB from './config/db.js';
+import { clerkMiddleware } from '@clerk/express';
+import {serve} from "inngest/express";
+import { inngest,functions } from './inngest/index.js';
+
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(clerkMiddleware());
 
 app.get('/', (req, res) => {
     res.send('Welcome to the Curtain Call API');
 });
+app.use("/api/inngest", serve({client:inngest,functions}));
 
 const PORT = process.env.PORT || 5000;
 
