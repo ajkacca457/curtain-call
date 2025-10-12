2496d957df06c7b5bb7d4107fb0e395824b1f3eb
Avijit Karmaker <ajkacca@gmail.com>
2025-10-12 08:25:53 +0300

    add dashboard data

---
+++ server/controllers/adminController.js
@@ -0,0 +1,39 @@
+import Booking from "../models/Booking.js";
+import ShowTime from "../models/ShowTime.js";
+import User from "../models/User.js";
+
+export const userIsAdmin= async (req,res,next)=> {
+    res.json({
+        success:true,
+        isAdmin:true
+    })
+}
+
+
+export const getAdminDashboardData= async (req,res,next)=> {
+
+    try {
+
+        const confirmedBookings= await Booking.find({isPaid:true});
+        const allActiveShows= await ShowTime.find({showDateTime:{$gte: new Date()}}).populate("show");
+        const totalUsers= await User.countDocuments();
+
+        const dashboardData= {
+            totalBookings: confirmedBookings.length,
+            totalRevenue: confirmedBookings.reduce((acc,booking)=> {
+                acc+ booking.amount
+            },0),
+            activeShowTImes: allActiveShows,
+            totalUsers
+        }
+
+        res.json({
+            success:true,
+            dashboardData
+        })
+
+        
+    } catch (error) {
+        next (error)
+    }
+}
