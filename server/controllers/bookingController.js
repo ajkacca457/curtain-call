71c078c1e7702c82ea320c017b069bb46dc52ac9
Avijit Karmaker <ajkacca@gmail.com>
2025-10-12 08:03:20 +0300

    add booking seats

--- server/controllers/bookingController.js
+++ server/controllers/bookingController.js
@@ -39,21 +39,21 @@ export const createBooking = async (req, res, next) => {
 
         const showTimeData = await ShowTime.findById(showTimeId).populate("show");
 
-        if(!showTimeData) {
-            return next(new ErrorResponse("show times are not available",404))
+        if (!showTimeData) {
+            return next(new ErrorResponse("show times are not available", 404))
         }
 
-        const booking = await Booking.create ({
+        const booking = await Booking.create({
             user: userId,
             showTime: showTimeId,
-            amount:showTimeData.showPrice*selectedSeats.length,
-            bookedSeats:selectedSeats
+            amount: showTimeData.showPrice * selectedSeats.length,
+            bookedSeats: selectedSeats
         })
 
         // update occupied seats 
 
-        selectedSeats.map((seat)=> {
-            showTimeData.occupiedSeats[seat]= userId;
+        selectedSeats.map((seat) => {
+            showTimeData.occupiedSeats[seat] = userId;
         })
 
         showTimeData.markModified('occupiedSeats');
@@ -62,13 +62,36 @@ export const createBooking = async (req, res, next) => {
 
         // strip gateway initialization
 
-res.status(200).json({
-  success: true,
-  message: "Booking successful",
-  bookingId: booking._id,
-  showTimeId,
-})
+        res.status(200).json({
+            success: true,
+            message: "Booking successful",
+            bookingId: booking._id,
+            showTimeId,
+        })
     } catch (error) {
         next(error);
+    }
+}
+
+
+export const getOccupiedSeats = async (req, res, next) => {
+
+    try {
+        const { showTimeId } = req.params;
+        const showTimeData = await ShowTime.findById(showTimeId);
+
+        if (!showTimeData) {
+            return next(new ErrorResponse(404, "show time is either expired or unavailable"));
+        }
+
+        const occupiedSeats = Object.keys(showTimeData.occupiedSeats);
+
+        res.json({
+            success: true,
+            occupiedSeats
+        })
+
+    } catch (error) {
+        next(error)
     }
 }
--- server/routes/bookingRoutes.js
+++ server/routes/bookingRoutes.js
@@ -1,10 +1,11 @@
 import express from "express"
-import { createBooking } from "../controllers/bookingController.js";
+import { createBooking, getOccupiedSeats } from "../controllers/bookingController.js";
 
 
 const router= express.Router();
 
-router.post("/create-booking",createBooking)
+router.post("/create-booking",createBooking);
+router.get("/seats/:shotTimeId");
 
 
 export default router;
