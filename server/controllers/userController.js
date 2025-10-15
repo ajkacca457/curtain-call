fb8f5d3ed58fc9bc05f043b38a2b885e459e2921
Avijit Karmaker <ajkacca@gmail.com>
2025-10-15 13:05:25 +0300

    add userbooking controller

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,10 +1,29 @@
 import Booking from "../models/Booking.js";
+import ErrorResponse from "../utils/ErrorHandle.js";
 
 
-export const userBooking= async(req,res,next)=> {
-    res.status(200).json({
-        success:true,
-        message: "will return all the user specific bookings"
-    })
+export const userBooking = async (req, res, next) => {
+
+    try {
+        const user = req.auth().userId;
+
+        const bookings = await Booking.find({ user }).populate({
+            path: "showTime",
+            populate: { path: "show" }
+        }).sort({ createdAt: -1 });
+
+        if (bookings.length < 1) {
+            return next(new ErrorResponse(404, "user has no available bookings"));
+        }
+
+        res.status(200).json({
+            success: true,
+            bookings
+        })
+
+
+    } catch (error) {
+        next(error)
+    }
 
 }
