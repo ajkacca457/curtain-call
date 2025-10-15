22847e0237f62cabddeaf887e22646ad726516d6
Avijit Karmaker <ajkacca@gmail.com>
2025-10-15 13:26:27 +0300

    add favorite function to user controller

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,5 +1,6 @@
 import Booking from "../models/Booking.js";
 import ErrorResponse from "../utils/ErrorHandle.js";
+import { clerkClient } from "@clerk/express";
 
 
 export const userBooking = async (req, res, next) => {
@@ -24,6 +25,41 @@ export const userBooking = async (req, res, next) => {
 
     } catch (error) {
         next(error)
+    }
+
+}
+
+
+export const addFavorite = async (req, res, next) => {
+    try {
+        const { showId } = req.body;
+        const userId = req.auth().userId;
+
+        const user = await clerkClient.users.getUser(userId);
+
+        if (!user) {
+            return next(new ErrorResponse(404, "User not found"));
+        }
+
+        if (!user.privateMetadata.favorites) {
+            user.privateMetadata.favorites = [];
+        }
+
+        if (!user.privateMetadata.favorites.includes(showId)) {
+            user.privateMetadata.favorites.push(showId);
+        }
+
+        await clerkClient.users.updateUserMetadata(userId, {
+            privateMetadata: user.privateMetadata
+        });
+
+        res.status(200).json({
+            success: true,
+            message: "Show is added to favorites successfully"
+        })
+
+    } catch (error) {
+        next(error);
     }
 
 }
