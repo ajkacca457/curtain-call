9e1c89ca30e3c03381ed675aae399e80b05b990e
Avijit Karmaker <ajkacca@gmail.com>
2025-10-15 13:53:30 +0300

    add get favorites function

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,10 +1,13 @@
 import express from "express"
-import {userBooking} from "../controllers/userController.js"
+import {userBooking, addFavorite, updateFavorite, getFavorites} from "../controllers/userController.js"
 
 
 const router= express.Router();
 
 
 router.get("/booking",userBooking);
+router.post("/add-favorite", addFavorite);
+router.post("/update-favorite",updateFavorite);
+router.get("/favorites", getFavorites);
 
 export default router;
