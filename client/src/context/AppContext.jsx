feab9d8c3b7b2c0b62fe3c7ce9618bfbce066b6d
Avijit Karmaker <ajkacca@gmail.com>
2025-10-25 10:09:16 +0300

    update fetch status

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,6 +1,9 @@
 import { createContext, useContext } from "react";
 import axios from "axios";
 import { useState, useEffect } from "react";
+import { useUser, useAuth } from "@clerk/clerk-react";
+import { Navigate } from "react-router-dom";
+import toast from "react-hot-toast";
 
 axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL;
 
@@ -12,6 +15,28 @@ export const AppProvider = ({ children }) => {
     const [isAdmin, setIsAdmin] = useState(false);
     const [shows, setShows] = useState([]);
     const [favorites, setFavorites] = useState([]);
+
+    const { user } = useUser();
+    const {getToken} = useAuth(); 
+
+    const fetchAdminStatus = async () => {
+       try {
+           const data = await axios.get(`/users/${user.id}/admin-status`, {
+               headers: {
+                   Authorization: `Bearer ${await getToken()}`,
+               },
+           });
+           setIsAdmin(data.isAdmin);
+
+           if(!data.isAdmin && location.pathname.startsWith("/admin")) {
+               <Navigate to="/" />;
+               toast.error("Access denied. Admins only.");
+           }
+
+       } catch (error) {
+            console.error("Error fetching admin status:", error);
+       }
+    };
 
 
   return (
