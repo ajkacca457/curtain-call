5d5615b240c6d85f805e3e03f02485f17ae5d3d9
Avijit Karmaker <ajkacca@gmail.com>
2025-10-25 09:59:56 +0300

    add initial states for context

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,12 +1,21 @@
 import { createContext, useContext } from "react";
+import axios from "axios";
+import { useState, useEffect } from "react";
+
+axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL;
+
 
 const AppContext = createContext();
 
-
 export const AppProvider = ({ children }) => {
 
+    const [isAdmin, setIsAdmin] = useState(false);
+    const [shows, setShows] = useState([]);
+    const [favorites, setFavorites] = useState([]);
+
+
   return (
-    <AppContext.Provider value={{}}>
+    <AppContext.Provider value={{ isAdmin, shows, favorites }}>
       {children}
     </AppContext.Provider>
   );
