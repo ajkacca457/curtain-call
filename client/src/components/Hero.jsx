b7aefdbf6815f89311c90728f8893caf9026f062
Avijit Karmaker <ajkacca@gmail.com>
2025-10-29 15:57:03 +0200

    check admin status

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,7 +1,10 @@
-import React from 'react'
 import HeroImg from '../assets/hero-display.jpg'
+import { useAppContext } from '../context/AppContext'
 
 const Hero = () => {
+  const {isAdmin } = useAppContext();
+
+console.log("Is Admin:", isAdmin);
   return (
     <div
       className='relative h-[80vh] bg-cover bg-center bg-no-repeat max-w-[1600px] mx-auto rounded-2xl'
