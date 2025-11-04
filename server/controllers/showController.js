bfd4cd9839666f8e8583fd91a7662691e54be87f
Avijit Karmaker <ajkacca@gmail.com>
2025-11-04 21:49:18 +0200

    update showcontroller

--- server/controllers/showController.js
+++ server/controllers/showController.js
@@ -61,6 +61,48 @@ export const createSingleShow = async (req, res, next) => {
     }
 }
 
+export const createShowTime = async (req, res, next) => {
+    try {
+        const { showId, showsInput, showPrice } = req.body;
+
+        const show = await Show.findById(showId);
+
+        if (!show) {
+            return next(new ErrorResponse(`${showId}:show is not a active show`), 400);
+        }
+
+        let showsToCreate = [];
+
+        showsInput.forEach(element => {
+            const showdate = element.date;
+            element.time.forEach(item => {
+                const showDateTime = `${showdate}T${item}`
+                const showtime = {
+                    showId,
+                    showDateTime: new Date(showDateTime),
+                    showPrice,
+                    occupiedSeats: {}
+                }
+                showsToCreate.push(showtime);
+            })
+
+        });
+
+        if (showsToCreate.length > 0) {
+            await ShowTime.insertMany(showsToCreate);
+        }
+        res.status(200).json({
+            success: true,
+            message: "new show times are added"
+
+        })
+
+    } catch (error) {
+        next(error)
+    }
+
+}
+
 export const getAllShowTime = async (req, res, next) => {
     try {
         // first get all the ShowTIme
