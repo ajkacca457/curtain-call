3d0029349dd10b37cee64bb8c503543ef71a5104
Avijit Karmaker <ajkacca@gmail.com>
2025-12-29 23:55:00 +0200

    update scrolltotop component

--- client/src/components/AppLayout.jsx
+++ client/src/components/AppLayout.jsx
@@ -1,6 +1,7 @@
 import { useLocation, Outlet } from "react-router-dom";
 import Navbar from "./Navbar.jsx";
 import Footer from "./Footer.jsx";
+import ScrollToTop from "./ScrollToTop.jsx";
 import { Toaster } from "react-hot-toast";
 
 const AppLayout = () => {
@@ -10,6 +11,7 @@ const AppLayout = () => {
   return (
     <>
       <Toaster position="top-right" reverseOrder={false} />
+      <ScrollToTop />
       {!isAdmin && <Navbar />}
       <Outlet />
       {!isAdmin && <Footer />}
