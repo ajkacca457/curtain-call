7a9ee051eb5f8c53a10a1c4fc4139dfa8b441eab
Avijit Karmaker <ajkacca@gmail.com>
2025-12-30 16:03:30 +0200

    fix favorite cards

--- client/src/components/ShowCard.jsx
+++ client/src/components/ShowCard.jsx
@@ -4,14 +4,14 @@ const ShowCard = ({ show }) => {
   return (
     <div className="bg-white rounded-xl overflow-hidden border border-gray-200 hover:shadow-md transition flex flex-col justify-between">
       <img
-        src={show.poster_path}
-        alt={show.title}
+        src={show?.poster_path}
+        alt={show?.title}
         className="w-full h-72 object-cover"
       />
       <div className="p-4">
-        <h3 className="text-lg font-semibold mb-1">{show.title}</h3>
+        <h3 className="text-lg font-semibold mb-1">{show?.title}</h3>
         <div className="text-sm text-gray-500 mb-2">
-          {show.genres.slice(0, 2).map((genre, index) => (
+          {show?.genres?.slice(0, 2).map((genre, index) => (
             <span key={genre.id}>
               {genre.name}
               {index < show.genres.slice(0, 2).length - 1 && ' • '}
@@ -19,8 +19,8 @@ const ShowCard = ({ show }) => {
           ))}
         </div>
         <div className="flex justify-between items-center text-sm text-gray-600">
-          <span>⭐ {show.vote_average.toFixed(1)}</span>
-          <span>{new Date(show.release_date).getFullYear()}</span>
+          <span>⭐ {show?.vote_average?.toFixed(1)}</span>
+          <span>{new Date(show?.release_date).getFullYear()}</span>
         </div>
 
         <Link className="mt-4 block w-fit px-4 py-2 bg-purple-600 text-white ml-auto rounded hover:bg-purple-700 transition cursor-pointer" to={`/shows/${show._id}`}  >
