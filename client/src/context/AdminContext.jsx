444d61687671b163aa1b33acbb9cb3e60b8e8da1
Avijit Karmaker <ajkacca@gmail.com>
2025-12-31 13:30:38 +0200

    update showlist component

--- client/src/context/AdminContext.jsx
+++ client/src/context/AdminContext.jsx
@@ -9,6 +9,7 @@ export const AdminProvider = ({ children }) => {
   const { getToken } = useAuth();
 
   const [dashboardData, setDashboardData] = useState(null);
+  const [showTimes, setShowTimes] = useState([]);
 
   const fetchDashboardData = async () => {
     try {
@@ -23,9 +24,32 @@ export const AdminProvider = ({ children }) => {
     }
   };
 
+  const fetchListShows = async () => {
+    try {
+      const token = await getToken();
+      if (!token) {
+        console.warn("No token found, skipping fetch.");
+        return;
+      }
+
+      const response = await api.get("/api/admin/all-shows", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      console.log("✅ Fetched shows:", response.data);
+      setShowTimes(response.data.showTimes || []);
+    } catch (error) {
+      console.error("❌ Error fetching shows:", error);
+    } 
+  };
+
   return (
     <AdminContext.Provider
       value={{
+        showTimes,
+        fetchListShows,
         dashboardData,
         fetchDashboardData,
       }}
