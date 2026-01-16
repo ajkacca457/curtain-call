c97769ba43194810eef34f56b0bf7255cf2b06f7
Avijit Karmaker <ajkacca@gmail.com>
2026-01-16 12:30:35 +0200

    fix scrollable container

--- client/src/components/admin/AdminLayout.jsx
+++ client/src/components/admin/AdminLayout.jsx
@@ -4,12 +4,15 @@ import AdminSidebar from "./AdminSidebar";
 
 const AdminLayout = () => {
   return (
-    <div>
+    <div className="h-screen overflow-hidden">
       <AdminNavbar />
-      <div className="grid grid-cols-5">
-        <AdminSidebar />
 
-        <div className="col-span-4">
+      <div className="grid grid-cols-5 h-[calc(100vh-64px)]">
+        <div className="col-span-1 h-full">
+          <AdminSidebar />
+        </div>
+
+        <div className="col-span-4 h-full overflow-y-auto bg-gray-50 pb-12">
           <Outlet />
         </div>
       </div>
