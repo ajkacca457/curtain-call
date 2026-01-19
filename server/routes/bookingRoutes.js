ca8b6047529c60c84f207357377db2fe4b3446d4
Avijit Karmaker <ajkacca@gmail.com>
2026-01-19 17:43:42 +0200

    update webhooks and get rid of raw error

--- server/routes/bookingRoutes.js
+++ server/routes/bookingRoutes.js
@@ -1,18 +1,12 @@
 import express from "express"
-import { createBooking, getOccupiedSeats, holdSeats, createStripeSession, stripeWebhookHandler } from "../controllers/bookingController.js";
+import { getOccupiedSeats, holdSeats, createStripeSession } from "../controllers/bookingController.js";
 import { requireAuth } from "@clerk/express";
 
 
 const router= express.Router();
 
 router.post("/hold-seats",holdSeats);
-router.post("/create-booking", requireAuth(),createBooking);
 router.post("/create-stripe-session", requireAuth(), createStripeSession);
-router.post(
-  "/confirm-webhook",
-  express.raw({ type: "application/json" }),
-  stripeWebhookHandler
-);
 router.get("/seats/:showTimeId",getOccupiedSeats);
 
 export default router;
